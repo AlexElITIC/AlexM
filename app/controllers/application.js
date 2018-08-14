9b56f1705d096c8b6cf95d7a6bae2bcbb7f7b906
AlexElITIC <A01271558@itesm.mx>
2018-08-14 08:26:47 -0500

    14/08/2018

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,32 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	cat: 'pelusa',
 
-	weekDays : [
-		'lunes',
-		'martes',
-		'miercoles',
-		'jueves',
-		'viernes',
-		'sabado',
-		'domingo'
-
-	],
-
-	actions:
-	 {
-		doSomething()
-		{
-			var cat= this.get('cat');
-			console.log(cat);
-		},
-
-		valdiarUsuario()
+		validarUsuario()
 		{
 			var usuario= this.get('user');
 			if(usuario=="A01271558" && this.get('password')==="Star-Lord10"){
-				console.log("usuario valido");
+				this.transitionToRoute('lab');
 			}
 			else {
 				console.log("Usuario no valido");
@@ -35,14 +15,6 @@ export default Ember.Controller.extend({
 			this.set('user',"");
 			this.set('password',"");
 
-		},
-
-
-		myLuckyDay(){
-			var weekDays= this.get('weekDays');
-			var n = Math.floor(Math.random() * weekDays.length);
-			console.log(weekDays.objectAt(n));
 		}
 
-	}
 });
---
+++ app/controllers/lab.js
@@ -0,0 +1,65 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	cat: 'pelusa',
+
+	weekDays : [
+		{day:'lunes',
+		number:1,
+		'mi propiedad':'hola'},
+		{day:'martes',
+		number:2,
+		'mi propiedad':'hola'},
+		{day:'miercoles',
+		number:3,
+		'mi propiedad':'hola'},
+		{day:'jueves',
+		number:4,
+		'mi propiedad':'hola'},
+		{day:'viernes',
+		number:5,
+		'mi propiedad':'hola'},
+		{day:'sabado',
+		number:6,
+		'mi propiedad':'hola'},
+		{day:'domingo',
+		number:7,
+		'mi propiedad':'hola'}
+	],
+tweets:[],
+	actions:
+	 {
+		doSomething()
+		{
+			var cat= this.get('cat');
+			console.log(cat);
+		},
+		myLuckyDay(){
+			var weekDays= this.get('weekDays');
+			var luckyDay=this.get('luckyDay');
+			var n = Math.floor(Math.random() * weekDays.length);
+			this.set('luckyDay',weekDays.objectAt(n)['day']);
+		},
+		addDay(){
+			var newDay=this.get('newDay');
+			var weekDays=this.get('weekDays');
+
+			weekDays.pushObject({
+				day:newDay,
+				//number:weekDays.length+1,
+				'mi propiedad':'hola'
+			});
+		},
+		addTweet(){
+			var newTweet=this.get('newTweet');
+			var tweets=this.get('tweets');
+
+			tweets.pushObject({
+				user:'Alex',
+				tweet:newTweet
+			});
+			this.set('newTweet',"");
+		}
+
+	}
+});
